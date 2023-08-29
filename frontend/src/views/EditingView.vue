48e544f2ac783067a437f36b88c45d1d810079c1
kaiyuanCui <kevincui2002@gmail.com>
2023-08-30 02:30:40 +1000

    implemented user inputs page

---
+++ frontend/src/views/EditingView.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="inputs-container">
+        {{ username }}
+        {{ textFile }}
+        {{ audioFile }}
+
+         
+    </div>
+
+</template>
+
+<script>
+
+import BackendService from "../services/backendService.ts"
+
+const backendService = new BackendService();
+// we use the Options API Style: https://vuejs.org/guide/introduction.html#single-file-components
+export default {
+    name: 'EditingView',
+    components:{
+     // add components here
+
+
+
+    },
+    props:{
+        username: {
+            type: String,
+        },
+        textFile: {
+            type: Blob
+        },
+        audioFile: {
+            type: Blob
+        },
+    },
+    data(){
+        return{
+            
+        }
+    },
+    computed:{
+       
+    },
+
+    methods:{
+        
+    },
+    created(){
+        console.log("editing view created!");
+   
+    },
+ 
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
