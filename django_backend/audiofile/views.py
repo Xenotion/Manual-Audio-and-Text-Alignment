92f1badaf2f3441d95b8f4f3804654f1129f9d10
Xenotion <junghyunan0319@gmail.com>
2023-09-18 15:28:10 +1000

    implemented API (POST and GET) requests in the backend

--- django_backend/audiofile/views.py
+++ django_backend/audiofile/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import AudioFile
-from .serialiser import AudioFileSerializer
-from rest_framework.generics import ListAPIView
+from .models import TextFile  # Make sure your model is adjusted for text files.
+from .serialiser import TextFileSerializer  # Adjust your serializer for text files.
 from django.views.decorators.csrf import csrf_exempt
 
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
+    def get(self, request):
+        # This method retrieves all the text files in the database
+        files = TextFile.objects.all()
+        file_serializer = TextFileSerializer(files, many=True)
+        return Response(file_serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request):
-        file_serializer = AudioFileSerializer(data=request.data)
+        # This method saves a text file from the frontend to the database
+        file_serializer = TextFileSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-class AudioFileListView(ListAPIView):
-    queryset = AudioFile.objects.all()
-    serializer_class = AudioFileSerializer
+
